cdd83041460de2c00644d3947e3c509393acca13
gvb110505-max <gvb110505@gmail.com>
2026-08-23 22:43:13 +0900

    Create [...path].js

---
+++ api/[...path].js
@@ -0,0 +1,7 @@
+const app = require("../index.js");
+
+module.exports = (req, res) => {
+  if (req.url === "/api" || req.url === "/api/") req.url = "/";
+  else if (req.url.startsWith("/api/")) req.url = req.url.slice(4);
+  return app(req, res);
+};
